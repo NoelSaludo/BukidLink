9544c732254b946a7c78bbfe6b47edb6d2d69283
JerzhaLalu <23-05464@g.batstate-u.edu.ph>
2025-11-01 21:47:33 +0800

    Added the OrderPage, OrderService, CheckOutPage, OrderDetails.

---
+++ lib/models/Order.dart
@@ -0,0 +1,39 @@
+import 'package:bukidlink/models/CartItem.dart';
+
+enum OrderStatus {
+  toPay,
+  toShip,
+  toReceive,
+  toRate,
+  completed,
+}
+
+class Order {
+  final String id;
+  final List<CartItem> items;
+  final String recipientName;
+  final String contactNumber;
+  final String shippingAddress;
+  final DateTime datePlaced;
+  DateTime? dateDelivered;
+  OrderStatus status;
+
+  Order({
+    required this.id,
+    required this.items,
+    required this.recipientName,
+    required this.contactNumber,
+    required this.shippingAddress,
+    required this.datePlaced,
+    this.dateDelivered,
+    required this.status,
+  });
+
+  /// Compute total amount from all cart items
+  double get total => items.fold(0, (sum, item) => sum + item.totalPrice);
+
+  /// Check if all products in this order are rated
+  bool get isAllRated {
+    return items.every((item) => item.product.tempRating > 0);
+  }
+}
