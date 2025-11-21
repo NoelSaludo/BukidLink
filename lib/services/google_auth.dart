78ef8daa1c857d6357a7b7874593818441869e42
Noel Saludo <noelsaludo17@gmail.com>
2025-11-21 18:18:02 +0800

    feat: implement Google Sign-In and Firestore user management

--- lib/services/google_auth.dart
+++ lib/services/google_auth.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -11,9 +12,78 @@ class FirebaseService {
 
   Future<UserCredential?> initSignIn() async {
     unawaited(_googleSignIn.initialize(
-      serverClientId: "774780104370-puug8e1ltv5kfa64ig7oq04i5galnfgs.apps.googleusercontent.com"
+        serverClientId: "774780104370-puug8e1ltv5kfa64ig7oq04i5galnfgs.apps.googleusercontent.com"
     ));
 
     isInitialized = true;
+  }
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      if (!isInitialized) {
+        await initSignIn();
+      }
+
+      final GoogleSignInAccount googleUser = await _googleSignIn
+          .authenticate();
+
+      final idToken = googleUser.authentication.idToken;
+      final authorizationClient = googleUser.authorizationClient;
+      GoogleSignInClientAuthorization? authorization = await authorizationClient
+      .authorizationForScopes(['email', 'profile']);
+
+      final accessToken = authorization?.accessToken;
+      if (accessToken == null ) {
+        final authorization2 = await authorizationClient
+            .authorizationForScopes(['email', 'profile']);
+
+        if (authorization2?.accessToken == null) {
+          throw FirebaseAuthException(code: "error", message: "error");
+        }
+
+        authorization = authorization2;
+      }
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: accessToken,
+        idToken: idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      final User? user = userCredential.user;
+
+      if (user == null) {
+        throw FirebaseAuthException(code: "error", message: "error");
+      }
+      final userDoc = FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid);
+      final userSnapshot = await userDoc.get();
+      if (!userSnapshot.exists) {
+        await userDoc.set({
+          'email': user.email,
+          'displayName': user.displayName,
+          'photoURL': user.photoURL,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      }
+
+      return userCredential;
+    }
+
+    catch (e) {
+      print("Error during Google Sign-In: $e");
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    try {
+    await _googleSignIn.signOut();
+    } catch (e) {
+      print("Error during Google Sign-Out: $e");
+    }
   }
 }
